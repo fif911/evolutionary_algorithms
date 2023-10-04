de3286ab1f055a70e04e49a52e6acdb851105fa0
Oleksandr Zakotianskyi <ozakotianskyi@pixsystem.com>
2023-10-04 13:31:45 +0200

    Boostrap framework for running algorithms

--- controller_generalist_demo.py
+++ controller_generalist_demo.py
@@ -6,14 +6,13 @@
 # karine.smiras@gmail.com     				                              			  #
 #######################################################################################
 
-# imports framework
-import sys,os
-
-from evoman.environment import Environment
-from demo_controller import player_controller
+import os
 
 # imports other libs
 import numpy as np
+
+from demo_controller import player_controller
+from evoman.environment import Environment
 
 experiment_name = 'controller_generalist_demo'
 if not os.path.exists(experiment_name):
@@ -24,21 +23,19 @@ n_hidden_neurons = 0
 
 # initializes environment for multi objetive mode (generalist)  with static enemy and ai player
 env = Environment(experiment_name=experiment_name,
-				  playermode="ai",
-				  player_controller=player_controller(n_hidden_neurons),
-		  		  speed="normal",
-				  enemymode="static",
-				  level=2)
+                  playermode="ai",
+                  player_controller=player_controller(n_hidden_neurons),
+                  speed="fastest",
+                  enemymode="static",
+                  level=2,
+                  logs="off",
+                  visuals=True)
 
 sol = np.loadtxt('solutions_demo/demo_all.txt')
-print('\n LOADING SAVED GENERALIST SOLUTION FOR ALL ENEMIES \n')
 
 # tests saved demo solutions for each enemy
 for en in range(1, 9):
-	
-	#Update the enemy
-	env.update_parameter('enemies',[en])
-
-	env.play(sol)
-
-print('\n  \n')
+    # Update the enemy
+    env.update_parameter('enemies', [en])
+    f, p, e, t = env.play(sol)
+    print(f" ----- Enemy {en} Player {p}; Enemy {e}; in {t} seconds. Won: {e == 0}")
