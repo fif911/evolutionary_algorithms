f08403f5ef4a64b7f1f346da35deabcd70ef9376
Oleksandr Zakotianskyi <ozakotianskyi@pixsystem.com>
2023-10-08 22:45:29 +0200

    add max f evals termination condition

--- cma_v2.py
+++ cma_v2.py
@@ -3,6 +3,9 @@ Install cma package:
 pipenv install cma
 or
 pip install cma
+
+For CMA recommended number of evaluations is 100 * n_genes which bring us to 265 * 100 = 26,500 evaluations
+We can do 100,000 in 3 hours, so this is still ok
 """
 
 import os
@@ -15,7 +18,9 @@ from utils import simulation, verify_solution, init_env
 
 N_GENERATIONS = 50
 POP_SIZE = 50
-ENEMIES = [6]
+MAX_EVALUATIONS = 100_000
+
+ENEMIES = [1, 2, 3, 4, 5, 6, 7, 8]
 MODE = "train"  # train or test
 
 n_hidden_neurons = 10
@@ -31,13 +36,15 @@ def solution_search(env, n_genes):
     es = cma.CMAEvolutionStrategy(n_genes * [0], 0.8,
                                   inopts={'bounds': [-1, 1],
                                           'popsize': POP_SIZE,
-                                          'maxiter': N_GENERATIONS})
+                                          # 'maxiter': N_GENERATIONS,
+                                          'maxfevals': MAX_EVALUATIONS,
+                                          })
 
     while not es.stop():
         X = es.ask()  # get list of new solutions
         fit = [simulation(env, x) for x in X]  # evaluate each solution
         print(
-            f"Generation {es.countiter}: Best fitness: {min(fit):.4f},\t mean: {np.mean(fit):.4f},\t "
+            f"Generation {es.countiter}:\t FEvals: {es.countevals},\t\t Best fitness: {min(fit):.4f},\t mean: {np.mean(fit):.4f},\t "
             f"worst: {max(fit):.2f},\t std: {np.std(fit):.1f}")
 
         # TODO: This is test mutation. Must be removed
