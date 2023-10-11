05a6537b2dc8aa2ac1b608df4f36a3528284ed6c
Oleksandr Zakotianskyi <ozakotianskyi@pixsystem.com>
2023-10-11 10:21:17 +0200

    add diversity_measures.py

--- sketches/diversity_measures.py
+++ sketches/diversity_measures.py
@@ -1,97 +1,65 @@
+import os
+
 import numpy as np
 import scipy
 
 
-def distance_matrix(population: np.array, metric: str):
+def calc_similarity_matrix(population: np.array, threshold: float = 1, metric: str = "euclidean"):
     """
     https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.pdist.html
 
     The distance metric to use. The distance function can be ‘braycurtis’, ‘canberra’, ‘chebyshev’, ‘cityblock’, ‘correlation’, ‘cosine’, ‘dice’, ‘euclidean’, ‘hamming’, ‘jaccard’, ‘jensenshannon’, ‘kulczynski1’, ‘mahalanobis’, ‘matching’, ‘minkowski’, ‘rogerstanimoto’, ‘russellrao’, ‘seuclidean’, ‘sokalmichener’, ‘sokalsneath’, ‘sqeuclidean’, ‘yule’.
+
+    for euclidial distance, 11 means they are diverse
+    lower than 0.5 means they are exactly similar
+    5 means they are similar but not exact (50% similar)
+
+    We choose lower than 1 as a threshold
+
+    returns similarity matrix with True/False values
     """
-    return scipy.spatial.distance.pdist(X=population, metric=metric)
+    condensed_matrix = scipy.spatial.distance.pdist(X=population, metric=metric)
+    square_matrix = scipy.spatial.distance.squareform(X=condensed_matrix)
+    return square_matrix < threshold  # return similarity matrix with True/False values
 
 
-def get_number_of_similar_solutions(population: np.ndarray, threshold: float = 0.1, metric: str = "euclidean"):
-    """Return the number of similar solutions in the population"""
-    distances = distance_matrix(population, metric=metric)
-    return np.sum(distances < threshold)
+def number_of_similar_solutions_per_individual(population, threshold, metric="euclidean"):
+    """Calculate the number of similar solutions per individual that are lower than the threshold
+    for euclidial distance, 11 means they are diverse
+    lower than 0.5 means they are exactly similar
+    5 means they are similar but not exact (50% similar)
 
 
-def get_similarity_matrix(population: np.ndarray, absolute_tolerance: float = 0.1):
-    """Return the number of similar solutions in the population using np.allclose
-    https://numpy.org/doc/stable/reference/generated/numpy.allclose.html#numpy-allclose
+    The resulting array will have the same length as the population and will show how many similar solutions
+    are there for each individual.
+    One is the minimum (the individual itself) and the maximum is the length of the
+    population - 1 (all other individuals are similar to the current one)
     """
-    similarity_matrix = np.zeros((len(population), len(population)))
-    for i, x in enumerate(population):
-        for j, y in enumerate(population):
-            similarity_matrix[i, j] = np.allclose(x, y, atol=absolute_tolerance)
-
-    return similarity_matrix
+    similarity_matrix = calc_similarity_matrix(population, threshold, metric)
+    return np.sum(similarity_matrix, axis=1)
 
 
-def genotypic_diversity(population):
-    """
-    Calculate genotypic diversity for a population of genotypes.
+def _get_population():
+    # Get np array of solutions for testing
+    solutions = []
+    for file in os.listdir('../solutions_beats_5_enemies'):
+        if file.startswith('pymoo'):
+            new_solution = []
+            with open(f'../solutions_beats_5_enemies/{file}') as f:
+                solutions.append(f.read().splitlines())
 
-    Parameters:
-    - population: A list of genotypes, where each genotype is a list of floats.
-
-    Returns:
-    - diversity_score: Genotypic diversity score (Shannon entropy).
-    """
-    num_individuals = len(population)
-    num_genes = len(population[0])
-
-    # Count the frequency of each unique gene across the population.
-    gene_frequencies = {}
-    for genotype in population:
-        for gene in genotype:
-            gene_frequencies[gene] = gene_frequencies.get(gene, 0) + 1
-
-    # Calculate Shannon entropy.
-    diversity_score = 0.0
-    for frequency in gene_frequencies.values():
-        probability = frequency / (num_individuals * num_genes)
-        diversity_score -= probability * np.log(probability)
-
-    return diversity_score
+    solutions = np.array(solutions, dtype=float)
+    return solutions
 
 
-def index_specific_genotypic_diversity(population):
-    """
-    Calculate diversity based on the occurrences of gene values at the same index in the population.
+if __name__ == '__main__':
+    # test different populations
+    population = _get_population()
+    population = np.random.uniform(low=-1, high=1, size=(100, 256))  # average euclidean distance is 13
+    population = np.random.normal(loc=0, scale=0.1, size=(100, 256))  # average euclidean distance is 2.2
+    population = np.random.normal(loc=0, scale=0.05, size=(100, 256))  # average euclidean distance is 1.1
 
-    Parameters:
-    - population: A list of genotypes, where each genotype is a list of floats.
+    similarity_matrix = calc_similarity_matrix(population, threshold=1, metric="euclidean")
+    per_individual = number_of_similar_solutions_per_individual(population, threshold=1, metric="euclidean")
 
-    Returns:
-    - diversity_score: Index-specific genotypic diversity score.
-    """
-    num_individuals = len(population)
-    num_genes = len(population[0])
-
-    # Count the number of unique gene values at each index.
-    unique_counts = [len(set(genotype[i] for genotype in population)) for i in range(num_genes)]
-
-    # Calculate the diversity score based on the number of unique values at each index.
-    diversity_score = sum(unique_counts) / num_genes
-
-    return diversity_score
-
-
-# Example usage:
-# Suppose you have a population of genotypes (lists of floats).
-population = [
-    [10, 0.4, 0.6, 1001],
-    [10, 0.4, 0.6, 1001],
-    [10, 0.4, 0.6, 1001],
-    [10, 0.3, 0.5, 100],
-    [10, 0.4, 0.6, 0.8],
-    [0.5, 0.5, 0.7, 10],
-]
-
-# Calculate genotypic diversity.
-diversity_score = index_specific_genotypic_diversity(population)
-
-# The diversity_score represents the genotypic diversity of the population.
-print(diversity_score)
+    print(per_individual)
