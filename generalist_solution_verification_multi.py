7a6bfb4c04743b2802785c25a372581b3462e935
Oleksandr Zakotianskyi <ozakotianskyi@pixsystem.com>
2023-10-15 19:41:06 +0200

    add tracking for 6 most performing

--- generalist_solution_verification_multi.py
+++ generalist_solution_verification_multi.py
@@ -13,7 +13,8 @@ initialise_script(experiment_name)
 env, ngenes = init_env(experiment_name, enemies=[1, 2, 3, 4, 5, 6, 7, 8], n_hidden_neurons=n_hidden_neurons)
 
 # solutions = read_solutions_from_file("magic_8", startswith="beats_8_")
-solutions = read_solutions_from_file("farmed_beats_8", startswith="beats_8")
+solutions = read_solutions_from_file("farmed_beats_8")
+# solutions = read_solutions_from_file("pymoo_sms_emoa")
 # solutions = np.concatenate((solutions, solutions_existing))
 
 # merged_solutions = np.loadtxt("BEST_SOLUTION_multi_8.txt")
@@ -27,6 +28,8 @@ max_health = 0
 its_time = 0
 win_id = None
 all_8_beating = True
+highest_p_life_6 = 0
+highest_p_life_6_id = 0
 for id, sol in enumerate(solutions):
     print(f"Solution {id}/{len(solutions)}")
     enemies_beaten, enemies_not_beaten, enemy_lives, player_lives, times = verify_solution(env, sol,
@@ -36,6 +39,10 @@ for id, sol in enumerate(solutions):
                                                                                            vv=True)
     if len(enemies_beaten) != 8:
         all_8_beating = False
+    highest_p_life_6 = max(highest_p_life_6, player_lives[5])
+    if highest_p_life_6 == player_lives[5]:
+        highest_p_life_6_id = id
+
     p_health = sum(player_lives)
     if p_health > max_health:
         max_health = p_health
@@ -50,10 +57,13 @@ print("---")
 print(f"Max health: {max_health}")
 print(f"In Time: {its_time}")
 print(f"Win id: {win_id}")
+print(f"Highest player life on enemy 6: {highest_p_life_6}")
+print(f"Highest player life on enemy 6 solution id: {highest_p_life_6_id}")
+
 if not all_8_beating:
     print("WARNING: Not all enemies beaten")
 else:
     print("All solutions beat 8 enemies")
 
 verify_solution(env, solutions[win_id], enemies=[1, 2, 3, 4, 5, 6, 7, 8])
-# np.savetxt(f'{experiment_name}/submission_solution.txt', solutions[win_id])
+np.savetxt(f'{experiment_name}/submission_solution.txt', solutions[win_id])
